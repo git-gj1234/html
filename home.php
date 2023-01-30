a55a3ec937906c004e4a7706c4f995aa12fe1940
unknown <mundrachetna@gmail.com>
2023-01-30 14:09:52 +0530

    gg chetna

--- home.php
+++ home.php
@@ -2,6 +2,9 @@
 <title>valmart</title>
 <head class = "a1">
     <link rel="stylesheet" type="text/css" href = "home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
 </head>
 <body>
@@ -89,5 +92,120 @@ if(mysqli_num_rows($retval)>0){
 
 mysqli_close($conn);
 ?>
+
+<footer id="footer">
+
+<div id="nav-container">
+    <button class="contact" id="nav-button">Contact Us</button>
+    <div id="nav-content">
+        <table style="width:100%;">
+            <tr>
+                <th style="width: 40%; ">
+                    Terms & Conditions
+                </th>
+                <th colspan="3" style="width: 60%;">
+                    Contact for any Queries
+                </th>
+            </tr>
+
+            <tr>
+                <td style="width:40%; ">
+                    <ul>
+                        <li>
+                            No Return policy is followed here.
+                        </li>
+                        <li>
+                            Only Amal, Chetna and GJ hold the rights to change anything here.
+                        </li>
+                        <li>
+                            Sir give full maks plzzzz. You are not welcome to use our website.
+                        </li>
+                    </ul>
+                </td>
+                <td style="width:20%;">
+                    <p>
+                        <p style="font-size:14px"><b>Customer Service : </b></p>
+                        <br>
+                        <span class="material-symbols-outlined">
+                            mail
+                            </span> :  amal.ai21@bmsce.ac.in
+                        <br>
+                        <span class="material-symbols-outlined">
+                            support_agent
+                            </span> : 8582852825
+                    </p>
+                </td>
+                <td style="width:20%;">
+                    <p>
+                        <p style="font-size:14px"><b>Delivery Service : </b></p>
+                        <br>
+                        <span class="material-symbols-outlined">
+                            mail
+                            </span> : adityagy.ai21@bmsce.ac.in
+                        <br>
+                        <span class="material-symbols-outlined">
+                            support_agent
+                            </span> : 8582852825
+                    </p>
+                </td>
+                <td style="width:20%;">
+                    <p>
+                        <p style="font-size:14px"><b>Helpline : </b></p>
+                        <br>
+                        <span class="material-symbols-outlined">
+                            mail
+                            </span> : chetna.ai21@bmsce.ac.in
+                        <br>
+                        <span class="material-symbols-outlined">
+                            support_agent
+                            </span> : 7004936699
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                      
+                      <div class="flex-center">
+                        <i class="fa fa-twitter fa-4x icon-3d"></i>
+                        <i class="fa fa-facebook fa-4x icon-3d"></i>
+                        <i class="fa fa-instagram fa-4x icon-3d"></i>
+                        <i class="fa fa-whatsapp fa-4x icon-3d"></i>
+                      </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4" style="text-align: center;">
+                    <p id="copy">
+                        Copyright© 2022, THG PUBLISHING PVT LTD. All Rights Reserved
+                    </p>
+
+                </td>
+            </tr>
+        </table>
+        
+    </div>
+  </div>
+</footer>
+  <script>
+  const button = document.querySelector("#nav-button");
+const content = document.querySelector("#nav-content");
+const footer = document.getElementById("footer");
+
+window.onscroll = function() {
+  if (window.pageYOffset > 50) {
+    footer.style.bottom = "0";
+  } else {
+    footer.style.bottom = "-50px";
+  }
+};
+
+button.addEventListener("click", function () {
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+});
+</script>
 
 </body>
